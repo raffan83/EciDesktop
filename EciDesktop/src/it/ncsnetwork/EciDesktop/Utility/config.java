679c2250b7c72b3fce26ba53c4adf3a26afd5586
MS <magostini@outlook.com>
2018-07-31 13:45:35 +0200

    add server demo url

--- EciDesktop/src/it/ncsnetwork/EciDesktop/Utility/config.java
+++ EciDesktop/src/it/ncsnetwork/EciDesktop/Utility/config.java
@@ -27,7 +27,9 @@ import javafx.stage.StageStyle;
  */
 public class config {
 	
-	public static final String URL_API = "http://localhost:8080/PortalECI/rest/";
+	//public static final String URL_API = "http://localhost:8080/PortalECI/rest/";
+	public static final String URL_API = "http://31.14.128.57:8096/PortalECI/rest/";
+	
 	public static final String RES_TEXT = "RES_TEXT";
 	public static final String RES_FORMULA = "RES_FORMULA";
 	public static final String RES_CHOICE = "RES_CHOICE";
